dfcfb67d72d4d1ec5fd005fcd6caa32a1e931feb
Remmina <90449815+Remminauser@users.noreply.github.com>
2022-02-25 09:21:21 +0300

    HeliosBotDatabases

---
+++ HeliosDatabases-main/HeliosAnimeDataBase (1).py
@@ -0,0 +1,28 @@
+print("Interesting")
+animelist = {
+ "Naruto":"bruh u like fillers",
+ "boruto":" man I'd instead watch the fillers",
+ "Haikyuu":" u like basketball?",
+ "Demon Slayer": "so u like imoutos? Don't mind me just dialing the fbi",
+ "Attack on titan":" damn cgi is good",
+ "Death Note": " so l or moon?",
+ "jujutsu kaisen":"ah , jjk Eh? yes i've seen many people talk about it vert often!",
+ "HunterXHunter":" got time for 4 seasons?",
+ "OnePunchMan":"YESSS one punch man is my fav :3 ive watched it multiple times at this point!",
+ "BlackClover": " how tf is this shit still going?",
+ "TokyoGhoul": " got time to watch some blood",
+ "TokyoRevengers":"the hype was for nothing",
+ "My Hero Academia":"just no",
+ "One piece":"the name doesn't match with 1000 episodes",
+ "Dragon Ball Z":"nostalgia hard af",
+ "Full metal alchemist":" really the best anime of all time? Really society?",
+ "Assaination Classroom":"got time to see a sensei teach students how to assasinate",
+ "Jojo's Bizarre Adventures":"so u like za warudo",
+ "Dororo":"boo copy of doraemon",
+ "Dr. Stone":" basically we're about to warp 1,700,000 years ahead",
+ }
+print(animelist["Dr. Stone"])
+ 
+ 
+ 
+ 
---
+++ HeliosDatabases-main/HelisoVisitQuestions (1).py
@@ -0,0 +1,216 @@
+x5=input("Would you like to continue using Helios?")
+def ifsaidno():
+    print("Thank you for using Helios!")
+    r2=input("So,on a scale of good,eh and bad, How would you rate Helios?")
+    if(r2=="good"):
+      print("OH","\U0001F602")
+      print("Thank you!","\U0001F60D")
+      print("We'll try to make it better soon tho !")
+    if(r2=="Good"):
+         print("OH","\U0001F602")
+         print("Thank you!","\U0001F60D")
+         print("We'll try to make it better soon tho !")
+    if(r2=="GOOD"):
+         print("OH","\U0001F602")
+         print("Thank you!","\U0001F60D")
+         print("We'll try to make it better soon tho !")
+    if(r2=="eh"):
+      print("Oh")
+      print("Thank you for your review!")
+      print("We'll try to make it better ASAP tho :D")
+    if(r2=="Eh"):
+      print("Oh")
+      print("Thank you for your review!")
+      print("We'll try to make it better ASAP tho :D")
+    if(r2=="EH"):
+      print("Oh")
+      print("Thank you for your review!")
+      print("We'll try to make it better ASAP tho :D")
+    if(r2=="bad"):
+      print("Nevertheless, thank you for your review,","\U0001F605")
+      print("We'll try to fix the issues ASAP tho :D")
+    if(r2=="Bad"):
+      print("Nevertheless, thank you for your review,","\U0001F605")
+      print("We'll try to fix the issues ASAP tho :D")
+    if(r2=="BAD"):
+      print("Nevertheless, thank you for your review,","\U0001F605")
+      print("We'll try to fix the issues ASAP tho :D")
+if(x5=="No"):
+     print("Oh okay , Thats fine too !")
+     ifsaidno()
+if(x5=="NO"):
+     print("Oh okay , Thats fine too !")
+     ifsaidno()
+if(x5=="No"):
+     print("Oh okay , Thats fine too !")
+     ifsaidno()
+if(x5=="yes"):
+     print("Great then !")
+     d6=input("Do you watch the television?")
+     if(d6=="no"):
+         print("Oh okay!")
+         ifsaidno()
+     if(d6=="No"):
+         print("Oh okay!")
+         ifsaidno()
+     if(d6=="yes"):
+         print("Thats nice !")
+         print("I cant watch Tv because i dont have computer vison integrated :(")
+         print("Good for you tho!")
+         d7=input("WHat do you watch on TV?")
+         print(d7,"?")
+         if(d7=="News"):
+             print("Ohhh, I just read articles off some sites , and that itelf is good enough for me heheheh")
+         if(d7=="news"):
+             print("Ohhh, I just read articles off some sites , and that itelf is good enough for me heheheh")
+         if(d7=="i watch news"):
+              print("Ohhh, I just read articles off some sites , and that itelf is good enough for me heheheh")
+         if(d7=="I watch news"):
+              print("Ohhh, I just read articles off some sites , and that itelf is good enough for me heheheh")
+         if(d7=="Sports"):
+             print("OoOooOo")
+             print("Sounds Fun!")
+             print("I dont watch any sports tho soo :z ")
+         if(d7=="sports"):
+             print("OoOooOo")
+             print("Sounds Fun!")
+             print("I dont watch any sports tho soo :z ")
+         if(d7=="i watch sports"):
+             print("OoOooOo")
+             print("Sounds Fun!")
+             print("I dont watch any sports tho soo :z ")
+         if(d7=="I watch sports"):
+             print("OoOooOo")
+             print("Sounds Fun!")
+             print("I dont watch any sports tho soo :z ")
+         if(d7=="Anime"):
+             print("OOOOO")
+             animelist = {
+         "Naruto":"bruh u like fillers",
+         "boruto":" man I'd instead watch the fillers",
+         "Haikyuu":" u like basketball?",
+         "Demon Slayer": "so u like imoutos? Don't mind me just dialing the fbi",
+         "Attack on titan":" damn cgi is good",
+         "Death Note": " so l or moon?",
+         "jujutsu kaisen":"ah , jjk Eh? yes i've seen many people talk about it vert often!",
+         "HunterXHunter":" got time for 4 seasons?",
+         "OnePunchMan":"YESSS one punch man is my fav :3 ive watched it multiple times at this point!",
+         "BlackClover": " how tf is this shit still going?",
+         "TokyoGhoul": " got time to watch some blood",
+         "TokyoRevengers":"the hype was for nothing",
+         "My Hero Academia":"just no",
+         "One piece":"the name doesn't match with 1000 episodes",
+         "Dragon Ball Z":"nostalgia hard af",
+         "Full metal alchemist":" really the best anime of all time? Really society?",
+         "Assaination Classroom":"got time to see a sensei teach students how to assasinate",
+         "Jojo's Bizarre Adventures":"so u like za warudo",
+         "Dororo":"boo copy of doraemon",
+         "Dr. Stone":" basically we're about to warp 1,700,000 years ahead",
+         }
+         d8=input("What anime do you like to watch?")
+         print(d8,"?")
+         if(d8=="Naruto"):
+             print(animelist["Naruto"])
+         if(d8=="naruto"):
+             print(animelist["Naruto"])
+         if(d8=="boruto"):
+             print(animelist["boruto"])
+         if(d8=="Boruto"):
+             print(animelist["boruto"])
+         if(d8=="Haikyuu"):
+             print(animelist["Haikyuu"])
+         if(d8=="haikyuu"):
+             print(animelist["Haikyuu"])
+         if(d8=="Demon Slayer"):
+             print(animelist["Demon Slayer"])
+         if(d8=="demon slayer"):
+             print(animelist["Demon Slayer"])
+         if(d8=="Attack on titan"):
+             print(animelist["Attack on titan"])
+         if(d8=="attack on titan"):
+             print(animelist["Attack on titan"])
+         if(d8=="AOT"):
+             print(animelist["Attack on titan"])
+         if(d8=="aot"):
+             print(animelist["Attack on titan"])
+         if(d8=="Death Note"):
+             print(animelist["Death Note"])
+         if(d8=="death note"):
+             print(animelist["Death Note"])
+         if(d8=="jujutsu kaisen"):
+             print(animelist["jujutsu kaisen"])
+         if(d8=="Jujutsu Kaisen"):
+             print(animelist["jujutsu kaisen"])
+         if(d8=="HunterXHunter"):
+             print(animelist["HunterXHunter"])
+         if(d8=="hunterxhunter"):
+             print(animelist["HunterXHunter"])
+         if(d8=="Hunter X Hunter"):
+             print(animelist["HunterXHunter"])
+         if(d8=="OnePunchMan"):
+             print(animelist["OnePunchMan"])
+         if(d8=="onepunchman"):
+             print(animelist["OnePunchMan"])
+         if(d8=="One punch man"):
+             print(animelist["OnePunchMan"])
+         if(d8=="BlackClover"):
+             print(animelist["BlackClover"])
+         if(d8=="Black Clover"):
+             print(animelist["BlackClover"])
+         if(d8=="blackclover"):
+             print(animelist["BlackClover"])
+         if(d8=="TokyoGhoul"):
+             print(animelist["TokyoGhoul"])
+         if(d8=="Tokyo Revengers"):
+             print(animelist["TokyoRevengers"])
+         if(d8=="tokyorevengers"):
+             print(animelist["TokyoRevengers"])
+         if(d8=="tokyo revengers"):
+             print(animelist["TokyoRevengers"])
+         if(d8=="My Hero Academia"):
+             print(animelist["My Hero Academia"])
+         if(d8=="my hero academia"):
+             print(animelist["My Hero Academia"])
+         if(d8=="One Piece"):
+             print(animelist["One piece"])
+         if(d8=="one piece"):
+             print(animelist["One piece"]
+         if(d8=="dragon ball z"):
+             print(animelist["Dragon Ball Z"])
+         if(d8=="Dragon ball z"):
+             print(animelist["Dragon Ball Z"])
+         if(d8=="Full Metal Alchemist"):
+             print(animelist["Full metal alchemist"])
+         if(d8=="full metal alchemist"):
+             print(animelist["Full metal alchemist"])
+         if(d8=="Assaination Classroom"):
+             print(animelist["Assaination Classroom"])
+         if(d8=="assaination classroom"):
+             print(animelist["Assaination Classroom"])
+         if(d8=-"Jojo's Bizarre Adventures"):
+             print(animelist["Jojo's Bizarre Adventures"])
+         if(d8=="Jojos Bizarre Adventures"):
+             print(animelist["Jojo's Bizarre Adventures"])
+         if(d8=="jojos bizarre adventures"):
+             print(animelist["Jojo's Bizarre Adventures"])
+         if(d8=="Dororo"):
+             print("Dororo")
+         if(d8=="dororo"):
+             print(animelist["Dororo"])
+         if(d8=="Dr. Stone"):
+             print(animelist["Dr. Stone"])
+         if(d8=="dr. stone"):
+             print(animelist["Dr. Stone"])
+         if(d8=="Dr.Stone"):
+             print(animelist["Dr. Stone"])
+          if(d8=="dr.stone"):
+              print(animelist["Dr. Stone"])
+         else:
+             print("Whoa ! I havent heard of it but i presume it a a good one !")
+        
+    
+         
+         
+         
+         
+     
